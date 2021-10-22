06d71d02e6a09abb9fa405f1a65398f56738db16
saraNersisian <saranersesian@gmail.com>
2021-10-22 15:59:34 -0700

    textfield was added to slid up panel

--- lib/Screens/Map/main_map_screen.dart
+++ lib/Screens/Map/main_map_screen.dart
@@ -1,6 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:sliding_up_panel/sliding_up_panel.dart';
-
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:wonder_flutter/Screens/Login/login_screen.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
@@ -34,44 +35,82 @@ class _MainMapScreenState extends State<MainMapScreen> {
     //       initialCameraPosition: _kInitialPosition,
     //     ),
     //
-    //   // appBar: AppBar(
-    //   //   title: Text('Google Maps Demo'),
-    //   // ),
+    //
     // );
     BorderRadiusGeometry radius = BorderRadius.only(
       topLeft: Radius.circular(24.0),
       topRight: Radius.circular(24.0),
-    );
-    return Material(
-      child: SlidingUpPanel(
-        backdropEnabled: true,
-        panel: Center(
-          child: Text (
-            "Create Thread + Chat"
-          ),
-        ),
-    collapsed: Container(
-    decoration: BoxDecoration(
-    color: Color(0xff22B4CF),
-    borderRadius: radius
 
-        ),
-      child: Center(
-        child: Text(
-          "Swip Up",
-          style: TextStyle(color: Colors.white),
+    );
+
+    return Material(
+        child: SlidingUpPanel(
+            backdropEnabled: true,
+            maxHeight: 300,
+            panel:Center (
+
+                child: _buildTextField(),
+              ),
+
+            collapsed:
+            Container(
+                  decoration: BoxDecoration(
+                    color: Color(0xff22B4CF),
+                    borderRadius: radius,
+                  ),
+
+                  child: Center(
+                    child: Text(
+                      "Swip Up to Create a Post",
+                      style: TextStyle(
+
+                        fontSize: 16,
+                        fontFamily: 'Poppins',
+                        color: Colors.white,
+
+                      ),
+
+                    ),
+
+                  ),
+                ),
+            borderRadius: radius,
+            margin: const EdgeInsets.fromLTRB(30.0, 30.0, 30.0, 0.0),
+            body: Scaffold(
+              body: GoogleMap(
+                initialCameraPosition: _kInitialPosition,
+              ),
+              // appBar: AppBar(
+              //     title: Text('Google Maps Demo'),
+              //   ),
+            )
+        )
+    );
+  }
+  Widget _buildTextField() {
+    final maxLines = 5;
+
+    return Container(
+
+      margin: EdgeInsets.all(30),
+      height: maxLines * 40.0,
+
+      child: TextField(
+        maxLines: maxLines,
+
+        decoration: InputDecoration(
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(20.0),
+            borderSide: BorderSide.none,
+          ),
+
+          labelText: "Enter a message ...",
+          fillColor: Color(0xffDDF4FF),
+          filled: true,
         ),
       ),
-    ),
-          borderRadius: radius,
-          body: Scaffold(
-          body: GoogleMap(
-                initialCameraPosition: _kInitialPosition,
-               ),
-          )
-      )
-      );
-
+    );
   }
+
 
 }
