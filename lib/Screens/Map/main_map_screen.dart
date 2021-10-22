1f734f75985730f07da07c7989c2d05b82eab4bf
saraNersisian <saranersesian@gmail.com>
2021-10-21 20:42:20 -0700

    Initial commit: added slide up panel to map page

--- lib/Screens/Map/main_map_screen.dart
+++ lib/Screens/Map/main_map_screen.dart
@@ -20,24 +20,58 @@ class MainMapScreen extends StatefulWidget {
 class _MainMapScreenState extends State<MainMapScreen> {
   TextStyle style = TextStyle(fontFamily: 'Montserrat', fontSize: 20.0);
   static final LatLng _kMapCenter =
-  LatLng(19.018255973653343, 72.84793849278007);
+  LatLng(34.143884, -118.253597);
 
   static final CameraPosition _kInitialPosition =
   CameraPosition(target: _kMapCenter, zoom: 11.0, tilt: 0, bearing: 0);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-
-
-        body: GoogleMap(
-          initialCameraPosition: _kInitialPosition,
-        ),
-
-      // appBar: AppBar(
-      //   title: Text('Google Maps Demo'),
-      // ),
+    // return Scaffold(
+    //
+    //
+    //     body: GoogleMap(
+    //       initialCameraPosition: _kInitialPosition,
+    //     ),
+    //
+    //   // appBar: AppBar(
+    //   //   title: Text('Google Maps Demo'),
+    //   // ),
+    // );
+    BorderRadiusGeometry radius = BorderRadius.only(
+      topLeft: Radius.circular(24.0),
+      topRight: Radius.circular(24.0),
     );
+    return Material(
+      child: SlidingUpPanel(
+        backdropEnabled: true,
+        panel: Center(
+          child: Text (
+            "Create Thread + Chat"
+          ),
+        ),
+    collapsed: Container(
+    decoration: BoxDecoration(
+    color: Color(0xff162E74),
+    borderRadius: radius
+
+        ),
+      child: Center(
+        child: Text(
+          "Swip Up",
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+    ),
+          borderRadius: radius,
+          body: Scaffold(
+          body: GoogleMap(
+                initialCameraPosition: _kInitialPosition,
+               ),
+          )
+      )
+      );
+
   }
 
 }
