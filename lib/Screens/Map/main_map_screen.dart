62ae7ba22b9bb62d96cb17c4f701f0d57cb7d43b
saraNersisian <saranersesian@gmail.com>
2021-11-06 14:22:21 -0700

    marker created successfully. pop up added to show the msg

--- lib/Screens/Map/main_map_screen.dart
+++ lib/Screens/Map/main_map_screen.dart
@@ -22,27 +22,73 @@ class MainMapScreen extends StatefulWidget {
 
 class _MainMapScreenState extends State<MainMapScreen> {
   TextStyle style = TextStyle(fontFamily: 'Montserrat', fontSize: 20.0);
-  static final LatLng _kMapCenter =
-  LatLng(34.143884, -118.253597);
+  static final LatLng _kMapCenter = LatLng(34.143884, -118.253597);
 
   static final CameraPosition _kInitialPosition =
-  CameraPosition(target: _kMapCenter, zoom: 11.0, tilt: 0, bearing: 0);
+   CameraPosition(target: _kMapCenter, zoom: 11.0, tilt: 0, bearing: 0);
 
-  // Set<Marker> _markers = {};
-  //
-  // void _onMapCreated(GoogleMapController controller){
-  //   setState(() {
-  //     _markers.add(
-  //       Marker(
-  //           markerId: MarkerId("id_1"),
-  //       position: _kMapCenter,
-  //       infoWindow: InfoWindow(
-  //         title: "Where is xyz?"
-  //       ),
-  //       ),
-  //     );
-  //   });
-  // }
+  Set<Marker> _markers = {};
+  late BitmapDescriptor mapMarker;
+
+
+  void setCustomMarker() async{
+    mapMarker = await BitmapDescriptor.defaultMarker;
+  }
+
+  var textEditingController = TextEditingController();
+  var displayThis;
+
+  void _onMapCreated(GoogleMapController controller){
+    setState(() {
+      _markers.add(
+        Marker(
+            markerId: MarkerId("id_1"),
+        position: _kMapCenter,
+        onTap: () {
+              showAlertDialog(context);
+
+        }
+        // infoWindow: InfoWindow(
+        //
+        //   title: displayThis,
+        // ),
+        ),
+      );
+    });
+  }
+
+  showAlertDialog(BuildContext context) {
+
+    // set up the button
+    Widget closeButton = TextButton(
+      child: Text("Close"),
+      onPressed: () { },
+    );
+    Widget replyButton = TextButton(
+      child: Text("Reply"),
+      onPressed: () { },
+    );
+
+    // set up the AlertDialog
+    AlertDialog alert = AlertDialog(
+      title: Text("I am wondering ..."),
+      content: Text(displayThis),
+      actions: [
+        replyButton,
+        closeButton,
+
+      ],
+    );
+
+    // show the dialog
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return alert;
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
 
@@ -52,111 +98,107 @@ class _MainMapScreenState extends State<MainMapScreen> {
 
     );
 
+
+
+
+    // return GestureDetector(
+    //   onTap:()=>FocusScope.of(context).unfocus(),
+    //   child: Material(
     return Material(
-
-
-    child: Column(
-
-
-       children:[
-
-         SlidingUpPanel(
-
-            backdropEnabled: true,
-            maxHeight: 180,
-            panel:Center (
-
-                child:Column(
-                  children:<Widget> [
-                    _buildTextField(),
-
-                  ElevatedButton(
-
-                    style: ElevatedButton.styleFrom(shape: new RoundedRectangleBorder(
-                      borderRadius: new BorderRadius.circular(100.0),
-
-
-                    ),
-                    ),
-
-                      onPressed: (){
-                  Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => MainMapScreen(title: 'main map page')),);
-                  },
-                      child: Text(
-                        "Post"
+      child: Column(
+         children:[
+           SlidingUpPanel(
+              backdropEnabled: true,
+              maxHeight: 180,
+              panel:Center (
+                  child:Column(
+                    children:<Widget> [
+                      _buildTextField(),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(shape: new RoundedRectangleBorder(
+                        borderRadius: new BorderRadius.circular(100.0),
+                         ),
                       ),
+                      onPressed: () {
+                        // Navigator.push(
+                        // context,
+                        // MaterialPageRoute(builder: (context) => MainMapScreen(title: 'main map page')),);
+                        // },
+                        displayThis = textEditingController.text;
+                        print(displayThis);
+                        // Navigator.push(
+                        // context,
+                        // MaterialPageRoute(builder: (context) => MainMapScreen(title: 'main map page')),);
+
+                      },
+                        child: Text(
+                          "Post"
+                        ),
+                    )
+                    ],
                   )
-                  ],
-                )
-
-              ),
-
-            collapsed:
-            Container(
-                  decoration: BoxDecoration(
-
-                    color: Color(0xff4CA3DD),
-                    borderRadius: radius,
-                  ),
-                  
-
-                  child: Center(
-
-                    child: Text(
-                      "Swip Up to Create a Post",
-                      style: TextStyle(
-
-                        fontSize: 16,
-                        fontFamily: 'Poppins',
-                        color: Colors.white,
-
-                      ),
-
+                ),
+              collapsed:
+              Container(
+                    decoration: BoxDecoration(
+                      color: Color(0xff33BDFF),
+                      borderRadius: radius,
                     ),
+                    child: Center(
+                      child: Text(
+                        "Swip Up to Create a Post",
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontFamily: 'Poppins',
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+              minHeight: 50,
+             // padding: EdgeInsets.all(20),
+              borderRadius: radius,
+              margin: const EdgeInsets.fromLTRB(30.0, 30.0, 30.0, 0.0),
+
+              body: Scaffold(
+                body:
+                GoogleMap(
+                       mapType: MapType.normal,
+                       initialCameraPosition: _kInitialPosition,
+                       myLocationEnabled: true,
+                       myLocationButtonEnabled: false,
+                       mapToolbarEnabled: true,
+                       zoomControlsEnabled: false,
+                  onMapCreated:_onMapCreated,
+                  markers: _markers,
+
+
+
+
+                   ),
+
+                floatingActionButton: Align(
+                  child: FloatingActionButton(
+
+
+                    onPressed: (){
+                      // Navigator.push(
+                      //   context,
+                      //   MaterialPageRoute(builder: (context) => ChatScreen(title: 'Chat page')),);
+                    },
+                    child: const Icon(Icons.chat),
+                    backgroundColor: Color(0xff33BDFF),
 
                   ),
-                ),
-            minHeight: 50,
-           // padding: EdgeInsets.all(20),
-            borderRadius: radius,
-            margin: const EdgeInsets.fromLTRB(30.0, 30.0, 30.0, 0.0),
-
-            body: Scaffold(
-              body:
-              GoogleMap(
-
-                     initialCameraPosition: _kInitialPosition,
-                     myLocationEnabled: true,
-                     myLocationButtonEnabled: false,
-                     mapToolbarEnabled: true,
-                     zoomControlsEnabled: false,
-
-
-
-                 ),
-
-              floatingActionButton: Align(
-                child: FloatingActionButton(
-
-
-                  onPressed: (){
-                    // Navigator.push(
-                    //   context,
-                    //   MaterialPageRoute(builder: (context) => ChatScreen(title: 'Chat page')),);
-                  },
-                  child: const Icon(Icons.chat),
-                  backgroundColor: Color(0xffC27BA0),
+                  alignment: Alignment(0.9,0.9),
 
                 ),
-                alignment: Alignment(0.9,0.9),
+
+                   ),
               ),
+            ],
+          ),
 
-                 ),
-            ),
-          ],
-        ),
 
     );
 
@@ -164,6 +206,7 @@ class _MainMapScreenState extends State<MainMapScreen> {
 
   Widget _buildTextField() {
     final maxLines = 5;
+    //TextEditingController _textEditingController = TextEditingController();
 
     return Container(
 
@@ -171,6 +214,9 @@ class _MainMapScreenState extends State<MainMapScreen> {
       height: maxLines * 20.0,
 
       child: TextField(
+
+        controller: textEditingController,
+
         maxLines: maxLines,
 
         decoration: InputDecoration(
@@ -184,6 +230,7 @@ class _MainMapScreenState extends State<MainMapScreen> {
           filled: true,
         ),
       ),
+
     );
   }
 
