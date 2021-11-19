d7b2b6b6fa0963167da59cb5dc975d788b0186c4
saraNersisian <saranersesian@gmail.com>
2021-11-18 20:45:08 -0800

    disabled the grey appbar

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@
 import 'package:flutter/material.dart';
 import 'Screens/Welcome/welcome_screen.dart';
 import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter/services.dart';
 
 
 
@@ -11,6 +12,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+    ));
     return MaterialApp(
       title: 'Wonder App',
       debugShowCheckedModeBanner: false,
@@ -18,7 +22,11 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         scaffoldBackgroundColor: Colors.white,
 
+
+
+
       ),
+
       home: WelcomeScreen(title: "Welcome Screen",),
     );
   }
